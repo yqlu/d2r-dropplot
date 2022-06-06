1b8ac5b1fb7c1ddc5524616cb1a5b29b8f65bdee
YQ Lu <yqlu@yqlu.me>
2022-06-05 21:48:12 -0400

    Update frontend to print out rarity

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
+import Fraction from "fraction.js";
+
 import { TCDict } from "./tc-dict";
 import { AtomicDict } from "./atomic-dict";
 import { ItemDict } from "./item-dict";
 import { makeLookupTcFunction, TcCalculator } from "./tc";
-import { TCResultAggregator } from "./resultAggregator";
+import {
+  BaseItemProbTuple,
+  BaseItemResultAggregator,
+  TCProbTuple,
+} from "./resultAggregator";
 import { sortTCs } from "./display";
+
+function format(percent: Fraction) {
+  return Math.round(percent.valueOf() * 10000) / 100;
+}
 
 function compute() {
   const tcName = $("#tcs option:selected").text();
+  const mlvl = parseInt($("#mlvl").val() as string);
+  const magicFind = parseInt($("#magicFind").val() as string);
   const partyCount = parseInt($("#partyCount").val() as string);
   const playerCount = parseInt($("#playerCount").val() as string);
 
@@ -14,13 +26,15 @@ function compute() {
   const tcLookup = makeLookupTcFunction(TCDict, AtomicDict);
   const tcCalculator = new TcCalculator(
     tcLookup,
-    () => new TCResultAggregator()
+    () => new BaseItemResultAggregator(mlvl, magicFind)
   );
   let tcs = tcCalculator.getAtomicTCs(tcName, partyCount, playerCount).result();
-  tcs = sortTCs(tcs);
+  tcs = sortTCs(
+    tcs as unknown as TCProbTuple[]
+  ) as unknown as BaseItemProbTuple[];
 
   let result = $("#result");
-  result.html("");
+  result.html("<table id='resulttable'><tr></tr></table>");
   for (var tcTuple of tcs) {
     if (ItemDict[tcTuple[0]]) {
       tcTuple[0] = `${ItemDict[tcTuple[0]].name} (${
@@ -36,8 +50,14 @@ function compute() {
     } else {
       f = Math.round(f);
     }
-    result.append(
-      `<div><span class='tcName'>${tcTuple[0]}</span><span class='tcValue'>${f}</span></div>`
+    $("#resulttable tr:last").after(
+      `<tr><td class='tcName'>${tcTuple[0]}</td>
+      <td class='tcValue'>1/${f}</td>
+      <td>${format(tcTuple[2].quality[0])}</td>
+      <td>${format(tcTuple[2].quality[1])}</td>
+      <td>${format(tcTuple[2].quality[2])}</td>
+      <td>${format(tcTuple[2].quality[3])}</td>
+      </tr>`
     );
   }
 }
