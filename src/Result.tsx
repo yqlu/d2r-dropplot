8e38bcd44859030c28832c29b9c718e676efdb15
YQ Lu <yqlu@yqlu.me>
2022-06-30 21:12:47 -0400

    Basic functionality to print out sets and uniques

--- src/Result.tsx
+++ src/Result.tsx
@@ -10,6 +10,20 @@ function format(percent: Fraction) {
   return Math.round(percent.valueOf() * 10000) / 100;
 }
 
+function formatReciprocal(chance: Fraction) {
+  if (chance.valueOf() == 0) {
+    console.log();
+    return `1/0`;
+  }
+  let f = chance.inverse().valueOf();
+  if (f < 50) {
+    f = Math.round(f * 10) / 10;
+  } else {
+    f = Math.round(f);
+  }
+  return `1/${f}`;
+}
+
 export class Result extends React.Component<IAppPropType, null> {
   render() {
     const tableRows = this.props.results.map((tcTuple) => {
@@ -19,21 +33,35 @@ export class Result extends React.Component<IAppPropType, null> {
       } else {
         name = tcTuple[0];
       }
-      let f = tcTuple[1].inverse().valueOf();
-      if (f < 50) {
-        f = Math.round(f * 10) / 10;
-      } else {
-        f = Math.round(f);
+      const children: JSX.Element[] = [];
+      for (const item of tcTuple[2].uniques) {
+        children.push(
+          <tr key={item[0]}>
+            <td>{item[0]} (U)</td>
+            <td>{formatReciprocal(item[1].mul(tcTuple[2].quality[3]))}</td>
+          </tr>
+        );
+      }
+      for (const item of tcTuple[2].sets) {
+        children.push(
+          <tr key={item[0]}>
+            <td>{item[0]} (S)</td>
+            <td>{formatReciprocal(item[1].mul(tcTuple[2].quality[2]))}</td>
+          </tr>
+        );
       }
       return (
-        <tr key={name}>
-          <td>{name}</td>
-          <td>1/{f}</td>
-          <td>{format(tcTuple[2].quality[0])}</td>
-          <td>{format(tcTuple[2].quality[1])}</td>
-          <td>{format(tcTuple[2].quality[2])}</td>
-          <td>{format(tcTuple[2].quality[3])}</td>
-        </tr>
+        <>
+          <tr key={name}>
+            <td>{name}</td>
+            <td>{formatReciprocal(tcTuple[1])}</td>
+            <td>{format(tcTuple[2].quality[0])}</td>
+            <td>{format(tcTuple[2].quality[1])}</td>
+            <td>{format(tcTuple[2].quality[2])}</td>
+            <td>{format(tcTuple[2].quality[3])}</td>
+          </tr>
+          {children}
+        </>
       );
     });
     return (
